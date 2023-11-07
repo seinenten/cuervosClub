a3357450a886a5d2ec015c1f5ccc1552ead3771a
seinenten <20200707@uthh.edu.mx>
2023-11-07 10:08:38 -0600

    Club de cuervos
    sistema de natacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AvisoPrivComponent } from './aviso-priv/aviso-priv.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'avisos',
+    component: AvisoPrivComponent
+  },
+  {
+    path: 'nosotros',
+    component: AcercaDeComponent
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'gestor',
+    loadChildren: () => import('./gestor/gestor.module').then(m => m.GestorModule)
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { AvisoPrivComponent } from './aviso-priv/aviso-priv.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ErrorPageComponent,
+    AvisoPrivComponent,
+    AcercaDeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
